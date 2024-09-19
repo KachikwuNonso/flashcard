d581396c829260a1cccadfe60ee82d64420a67e6
Kachikwu Chukwunonso <kachikwuchukwunonso726@gmail.com>
2024-09-19 21:31:13 +0100

    add the flex class

--- js/script.js
+++ js/script.js
@@ -27,3 +27,33 @@ const code = document.querySelector('#fifth');
 code.addEventListener('click', function () {
     code.classList.toggle('is-flipped');
 })
+
+const pen = document.querySelector('#six');
+
+pen.addEventListener('click', function () {
+    pen.classList.toggle('is-flipped');
+})
+
+const parameters = document.querySelector('#seventh');
+
+parameters.addEventListener('click', function () {
+    parameters.classList.toggle('is-flipped');
+})
+
+const turn = document.querySelector('#eighth');
+
+turn.addEventListener('click', function () {
+    turn.classList.toggle('is-flipped');
+})
+
+const chaining = document.querySelector('#ninth');
+
+chaining.addEventListener('click', function () {
+    chaining.classList.toggle('is-flipped');
+})
+
+const strings = document.querySelector('#tenth');
+
+strings.addEventListener('click', function () {
+    strings.classList.toggle('is-flipped');
+})
